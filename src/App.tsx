bda9ff3e32403c839d9483f40db1e1a0c230d326
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-09 02:42:25 -0300

    feat: Add todo render with map function to App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { todosData } from "./data/todos";
 
 
 function App() {
@@ -7,8 +8,15 @@ function App() {
     <h1 className="font-bold text-3xl text-center">
       Todo App
     </h1>
+    <div className="max-w-lg mx-auto">
+      <div className="space-y-2">
+        {todosData.map(todo => (
+          <p key={todo.id} className="text-lg">{todo.title}</p>
+        ))}
+      </div>
+    </div>
    </main>
   )
 }
 
-export default App
+export default App;
