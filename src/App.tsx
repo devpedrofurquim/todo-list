903aabe2fb29bc9d9779824742a7761e8df38e4d
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-09 15:02:56 -0300

    feat: Add onCompletedChange prop to TodoItem component. Add Alert to debug Todos on App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { todosData } from "./data/todos";
 
 function App() {
 
+  function setTodoCompleted(id: number, completed: boolean) {
+    alert(`Todo with id ${id} is now ${completed ? "completed" : "active"}`)
+  }
+
   return (
    <main className="p-10 h-screen space-y-5">
     <h1 className="font-bold text-3xl text-center">
@@ -12,7 +16,11 @@ function App() {
     <div className="max-w-lg mx-auto bg-slate-100 rounded p-5 border-gray-400 border-2">
       <div className="space-y-2">
         {todosData.map(todo => (
-          <TodoItem todo={todo}/>
+          <TodoItem
+          key={todo.id}
+          todo={todo}
+          onCompletedChange={setTodoCompleted}
+          />
         ))}
       </div>
     </div>
