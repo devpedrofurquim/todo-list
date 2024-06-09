239e36723fe37da3c1c5fb9d1c1be1082bb1a8c7
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-09 14:45:22 -0300

    feat: Design App background
    Design a prettier background to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { todosData } from "./data/todos";
 function App() {
 
   return (
-   <main className="p-10 h-screen">
+   <main className="p-10 h-screen space-y-5">
     <h1 className="font-bold text-3xl text-center">
       Todo App
     </h1>
-    <div className="max-w-lg mx-auto">
+    <div className="max-w-lg mx-auto bg-slate-100 rounded p-5 border-gray-400 border-2">
       <div className="space-y-2">
         {todosData.map(todo => (
           <TodoItem todo={todo}/>
