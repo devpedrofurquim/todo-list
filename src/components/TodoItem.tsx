bf8c8871fe3157187d00f227a42541207916d371
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-09 17:34:24 -0300

    feat: Move Todo List from App.tsx to TodoList.tsx
    Implement Sort Algorithm to List on TodoList component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,17 +3,24 @@ import { Todo } from "../types/todo";
 interface TodoItemProps {
     todo: Todo;
     onCompletedChange: (id: number, completed: boolean) => void;
+    onDelete: (id: number) => void;
 }
 
-const TodoItem = ({todo, onCompletedChange}: TodoItemProps) => {
+const TodoItem = ({todo, onCompletedChange, onDelete}: TodoItemProps) => {
     return ( 
     <div>
-        <label className="flex items-center gap-2 rounded-md border p-2 border-gray-400 bg-white hover:bg-slate-50">
+        <label className="flex justify-between items-center gap-2 rounded-md border p-2 border-gray-400 bg-white hover:bg-slate-50">
+            <div className="flex gap-2">
             <input 
             type="checkbox"
             onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
             className="scale-125"/>
             <span className={todo.completed ? "line-through text-gray-400" : ""}>{todo.title}</span>
+            </div>
+            <button 
+            className="hover:text-red-500 scale-125 px-2"
+            onClick={() => onDelete(todo.id)}
+            ><span>X</span></button>
         </label>
     </div> 
     );
